c46d9c948875dc6e913ec17ddb766ea1e9926890
Bhujay Kumar Bhatta <bhujay.bhatta@yahoo.com>
2024-04-02 08:28:38 +0530

    starting to parameterize which mha to use

--- src/encoder_layer.py
+++ src/encoder_layer.py
@@ -9,6 +9,60 @@ from add_norm import AddNorm
 from positionwise_ff import FeedForward
 
 
+
+
+class EncoderLayerV5(Layer):
+    ''' parameterised selection of MHA'''
+    def __init__(self, d_model, num_heads, dff,
+                 en_dropout=0.1,
+                 epsilon=1e-6, debug=False, 
+                 use_masked_softmax=False,
+                 row_mask_to_sero=False,
+                 use_bbmha=True,
+                 **kwargs):
+        super(EncoderLayerV2, self).__init__(**kwargs)
+        if use_bbmha:
+            self.mha = MultiHeadAttentionV3(num_heads, d_model, 
+                                            use_masked_softmax=use_masked_softmax,
+                                             row_mask_to_sero=row_mask_to_sero,
+                                            debug=debug)
+        else:
+            self.mha = tf.keras.layers.MultiHeadAttention(num_heads, key_dim=d_model)
+        self.add_norm1 = AddNorm(epsilon=epsilon)
+        self.add_norm2 = AddNorm(epsilon=epsilon)
+        self.ffn = FeedForward(d_model, dff)
+        self.en_dropout = en_dropout
+        self.dropout1 = Dropout(en_dropout)
+        self.dropout2 = Dropout(en_dropout)
+        self.debug=debug
+        self.supports_masking = True  # Declare that this layer supports masking
+        self.attention_weights = None
+        self.use_bbmha = use_bbmha
+        
+
+    def call(self, x, training=False, num_heads=1, mask=None):
+        if self.debug: print('mask received at encoder layer:', mask.shape)
+        if self.use_bbmha:
+            attn_output  = self.mha(x, x, x, num_heads=num_heads, mask=mask) 
+        else:
+            attn_output  = self.mha(query=x, value=x, key=x, return_attention_scores=False)
+        self.attention_weights = self.mha.attention_weights
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.add_norm1(x, attn_output)
+        ffn_output = self.ffn(out1)
+        # ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.add_norm2(out1, ffn_output)
+        self.debug = False
+        return out2
+
+
+
+
+
+
+
+
 class EncoderLayerV2(Layer):
     def __init__(self, d_model, num_heads, dff,
                  en_dropout=0.1,
